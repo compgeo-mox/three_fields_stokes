e5e8729cf48e4e52b4c3b994f4d532d96f7eb53c
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2022-08-01 15:03:40 +0200

    stokes class for 2d and 3d

--- src/stokes.py
+++ src/stokes.py
@@ -2,36 +2,53 @@ import scipy.sparse as sps
 import porepy as pp
 import pygeon as pg
 
-
-class Stokes:
-    def __init__(self, keyword, g):
+class Stokes2D:
+    def __init__(self, keyword):
         self.keyword = keyword
-        self.g = g
-        self.nd1 = pg.Nedelec1(keyword)
-        self.rt0 = pp.RT0(keyword)
 
-    def discretize(self, data):
-        data[pp.DISCRETIZATION_MATRICES] = {self.keyword: {}}
+    def matrix_and_rhs(self, mdg, vector_source):
+        # discretize
+        mass_rt0 = pg.face_mass(mdg)
+        M = pg.numerics.innerproducts.lumped_mass_matrix(mdg, 2, None)
 
-        self.nd1.discretize(self.g, data)
-        self.M = data[pp.DISCRETIZATION_MATRICES][self.keyword]["mass"]
+        curl = mass_rt0 * pg.curl(mdg)
+        div = pg.div(mdg)
 
-        self.rt0.discretize(self.g, data)
-        mass_rt0 = data[pp.DISCRETIZATION_MATRICES][self.keyword]["mass"]
+        # assemble the saddle point problem
+        A = curl * sps.linalg.spsolve(M.tocsc(), curl.T)
+        spp = sps.bmat([[A, -div.T], [div, None]], format="csc")
 
-        self.curl = mass_rt0 * data[pp.DISCRETIZATION_MATRICES][self.keyword]["curl"]
-        self.div = pg.div(self.g)
+        # assemble the right-hand side
+        proj = pg.proj_cells_to_faces(mdg)
+        rhs = sps.lil_matrix((spp.shape[0], 1))
+        rhs[:proj.shape[0]] = proj * vector_source
 
-    def assemble_hybridized(self, data):
-        A = self.curl * sps.linalg.spsolve(self.M.tocsc(), self.curl.T)
-        return sps.bmat([[A, -self.div.T], [self.div, None]], format="csc")
+        return spp, rhs, proj
 
-    def assemble(self):
-        return sps.bmat(
-            [
-                [self.M, -self.curl.T, None],
-                [self.curl, None, -self.div.T],
-                [None, self.div, None],
-            ],
-            format="csc",
-        )
+class Stokes3D:
+    def __init__(self, keyword):
+        self.keyword = keyword
+
+    def matrix_and_rhs(self, mdg, vector_source):
+        # discretize
+        discr_ne1 = pg.Nedelec1(self.keyword)
+        M = pg.numerics.innerproducts.lumped_mass_matrix(mdg, 2, discr_ne1)
+
+        curl_ne = sps.bmat([[discr_ne1.assemble_curl(sd) for sd in mdg.subdomains()]])
+        curl = pg.face_mass(mdg) * curl_ne
+        div = pg.div(mdg)
+
+        # assemble the saddle point problem
+        print("do the hybridization")
+        A = curl * sps.linalg.spsolve(M.tocsc(), curl.T.tocsc())
+        print("done")
+        spp = sps.bmat([[A, -div.T], [div, None]], format="csc")
+
+        # assemble the right-hand side
+        print("project the shit")
+        proj = pg.proj_cells_to_faces(mdg)
+        print("done")
+        rhs = sps.lil_matrix((spp.shape[0], 1))
+        rhs[:proj.shape[0]] = proj * vector_source
+
+        return spp, rhs, proj
